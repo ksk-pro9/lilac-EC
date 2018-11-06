3fa181af99054dd8ade9567b4eec7738c92ac332
onohiroyuki <hhhonooo@gmail.com>
2018-11-06 17:06:25 +0900

    ono1706

---
+++ src/com/internousdev/lilac/action/CreateUserCompleteAction.java
@@ -0,0 +1,115 @@
+package com.internousdev.lilac.action;
+
+import java.util.Map;
+
+import com.internousdev.lilac.dao.UserInfoDAO;
+import com.internousdev.lilac.dto.UserInfoDTO;
+
+
+public class CreateUserCompleteAction {
+
+	private String familyName;
+	private String firstName;
+	private String familyNameKana;
+	private String firstNameKana;
+	private String sex;
+	private String email;
+	private String loginId;
+	private String password;
+	private String categoryId;
+	private Map<String, Object> session;
+
+	public String execute(){
+
+
+
+
+		UserInfoDAO userInfoDAO = new UserInfoDAO();
+				UserInfoDTO userInfoDTO = UserInfoDAO.userLogin(loginId);
+				session.put("loginId", userInfoDTO,getLoginId());
+			}
+			session.put("logined", 1);
+		}
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFamilyNameKana() {
+		return familyNameKana;
+	}
+
+	public void setFamilyNameKana(String familyNameKana) {
+		this.familyNameKana = familyNameKana;
+	}
+
+	public String getFirstNameKana() {
+		return firstNameKana;
+	}
+
+	public void setFirstNameKana(String firstNameKana) {
+		this.firstNameKana = firstNameKana;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+}
