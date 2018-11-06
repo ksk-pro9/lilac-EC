310c8d7007cd77861ce05541d81eeed97bb8a318
Tomomi1022 <ttt1.2.3iwm@gmail.com>
2018-11-06 15:42:42 +0900

    11/6 iwamoto

--- src/com/internousdev/lilac/action/LoginAction.java
+++ src/com/internousdev/lilac/action/LoginAction.java
@@ -1,7 +1,7 @@
 package com.internousdev.lilac.action;
 
-import java.sql.SQLEsception;
-import java.sql.ArrayList;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +12,7 @@ import com.internousdev.lilac.dao.CartInfoDAO;
 import com.internousdev.lilac.dao.DestinationInfoDAO;
 import com.internousdev.lilac.dao.MCategoryDAO;
 import com.internousdev.lilac.dao.UserInfoDAO;
-import com.internousdev.lilac.dto.DestinationInfo;
+import com.internousdev.lilac.dto.DestinationInfoDTO;
 import com.internousdev.lilac.dto.MCategoryDTO;
 import com.internousdev.lilac.util.InputChecker;
 import com.opensymphony.xwork2.ActionSupport;
@@ -25,7 +25,7 @@ public class LoginAction extends ActionSupport implements SessionAware{
 	
 	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
 	private List<String> loginIdErrorMessageList = new ArrayList<String>();
-	private List<Stirng> passwordErrorMessageList = new ArrayList<String>();
+	private List<String> passwordErrorMessageList = new ArrayList<String>();
 	
 	private Map<String, Object> session;
 	
@@ -42,7 +42,76 @@ public class LoginAction extends ActionSupport implements SessionAware{
 		}
 		
 		InputChecker inputChecker = new InputChecker();
-
+		loginIdErrorMessageList = inputChecker.doCheck("ログインID", loginId, 1, 8, true, false, false, true, false, false, false);
+		passwordErrorMessageList = inputChecker.doCheck("パスワード", password, 1, 16, true, false, false, true, false, false, false);
+		
+		if(loginIdErrorMessageList.size()!=0 && passwordErrorMessageList.size()!=0) {
+			session.put("loginIdErrorMessageList", loginIdErrorMessageList);
+			session.put("passwordErrorMessageList", passwordErrorMessageList);
+			session.put("mCategoryDtoList", 0);
+		}
+		
+		if(!session.containsKey("mCategoryList")) {
+			MCategoryDAO mCategoryDao = new MCategoryDAO();
+			mCategoryDtoList = mCategoryDao.getMCategoryList();
+			session.put("mCategoryDtoList", mCategoryDtoList);
+		}
+		
+		UserInfoDAO userInfoDao = new UserInfoDAO();
+		if(userInfoDao.isExistsUserInfo(loginId, password)) {
+			if(userInfoDao.login(loginId, password) >0 ) {
+				UserInfoDTO userInfoDTO = userInfoDao.getUserInfo(loginId, password);
+				session.put("loginId", userInfoDTO.getUserId());
+				int count=0;
+				CartInfoDAO cartInfoDao = new CartInfoDAO();
+				
+				count = cartInfoDao.linkToLoginId(String.valueOf(session.get("tempUserId")), loginId);
+				if(count > 0) {
+					DestinationInfoDAO destinationInfoDao = new DestinationInfoDAO();
+					try {
+						List<DestinationInfoDTO> destinationInfoDtoList = new ArrayList<DestinationInfoDTO>();
+						destinationInfoDtoList = destinationInfoDao.getDestinationInfo(loginId);
+						Iterator<DestinationInfoDTO> iterator = destinationInfoDtoList.iterator();
+						if(!(iterator.hasNext())) {
+							destinationInfoDtoList = null;
+						}
+						session.put("destinationInfoDtoList", destinationInfoDtoList);
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+					result = "settlement";
+				}else {
+					result = SUCCESS;
+				}
+			}
+			session.put("logined", 1);
+		}
+		return result;
+	}
+	
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCtegoryId(String categoryId) {
+		this.categoryId= categoryId;
+	}
+	public String getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Map<String, Object> getSession(){
+		return session;
+	}
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
 
 }
